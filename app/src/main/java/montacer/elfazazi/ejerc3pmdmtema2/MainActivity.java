cb2b6680ff158785faca8d796b01964fb5230a71
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-04 10:06:13 +0100

    Initial commit

---
+++ app/src/main/java/montacer/elfazazi/ejerc3pmdmtema2/MainActivity.java
@@ -0,0 +1,30 @@
+package montacer.elfazazi.ejerc3pmdmtema2;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import montacer.elfazazi.ejerc3pmdmtema2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
